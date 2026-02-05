63212d26dbdbb79a8597602ed916936fe0381688
Mir_Mac <mirdraco@Mir-Macui-MacBookAir.local>
2026-02-05 15:36:40 +0900

    2026.02.05

---
+++ README.md
@@ -0,0 +1,4 @@
+# Mir & Melty's Project
+
+## 📅 작업 내역
+- **2026-02-03** : FurGall 파일 생성
---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+app.use(express.static('public/html', { extensions: ['html'] })); // .html 확장자 생략 가능하게 설정
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/html/index.html');
+});
+
+// 404 에러 처리 (맨 마지막에 위치해야 함)
+app.use((req, res, next) => {
+  res.status(404).send('<h1>404 Not Found</h1><p>페이지를 찾을 수 없습니다.</p>');
+});
+
+app.listen(port, () => {
+  console.log(`서버가 http://localhost:${port} 에서 실행 중입니다`);
+});
